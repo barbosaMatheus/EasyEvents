ec33b76ad5148ecb0ae60567233470465a2f7db7
Ryan Thomas McIver <rmciver@mac10-cs.ms-2345.okstate.edu>
2016-10-24 18:02:58 -0500

    Initial Commit

---
+++ EasyEvents/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  EasyEvents
+//
+//  Created by Ryan Thomas McIver on 10/24/16.
+//  Copyright © 2016 Oklahoma State University. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
